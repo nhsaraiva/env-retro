c7f4a21a74868faac970f1369407765f51ab9c82
Nathanael Saraiva <natham_11@hotmail.com>
2022-04-27 05:07:05 -0300

    test coverage 100%

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    clearMocks: true,
-    coverageDirectory: "coverage",
-    moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
-    testEnvironment: "node",
-    preset: 'ts-jest',
-  };
